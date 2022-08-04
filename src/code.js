a70c2d5de3f730bd5e7bd0940fa66aaffbf61b5c
ririka-kabeshita <ririka.kabeshita@esm.co.jp>
2022-08-04 15:09:52 +0900

    [fix] 登録したら画面が遷移されてしまうバグ解消

--- src/code.js
+++ src/code.js
@@ -1,9 +1,9 @@
 function doGet() {
-    return HtmlService.createTemplateFromFile('src/index').evaluate();
+  return HtmlService.createTemplateFromFile("src/index").evaluate();
 }
 
 function createCompanyCalendar(companyName) {
-  const options='{createdBy:"就活スケジュール管理 ",aspiration:"後で追加"}';
-  const calendar=CalendarApp.createCalendar(companyName);
+  const options = '{createdBy:"就活スケジュール管理 ",aspiration:"後で追加"}';
+  const calendar = CalendarApp.createCalendar(companyName);
   calendar.setDescription(options);
 }
