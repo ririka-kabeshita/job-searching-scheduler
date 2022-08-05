e15925603ed8406c776cefacb14e5b53bd138ed7
ririka-kabeshita <ririka.kabeshita@esm.co.jp>
2022-08-05 09:02:55 +0900

    [fix] 未入力の時の警告が出ないバグを修正 [update] 存在する企業を入力した場合アラート表示

--- src/code.js
+++ src/code.js
@@ -3,7 +3,35 @@ function doGet() {
 }
 
 function createCompanyCalendar(companyName) {
-  const options = '{createdBy:"就活スケジュール管理 ",aspiration:"後で追加"}';
-  const calendar = CalendarApp.createCalendar(companyName);
-  calendar.setDescription(options);
+  const result = checkSameCompany(companyName);
+  if (result == true) {
+    const options = '{createdBy:"就活スケジュール管理 ",aspiration:"後で追加"}';
+    const calendar = CalendarApp.createCalendar(companyName);
+    calendar.setDescription(options);
+    return true;
+  } else {
+    return null;
+  }
+}
+/**
+ * 入力された値がカレンダーに存在する企業出ないかをチェックしたい
+ * 　カレンダーを全て取得する
+ * 　カレンダーの中で、”就活スケジュール管理”と説明文が入ってるものだけを取得する
+ * 　その中から、同じ名前がないかを確認する
+ */
+function checkSameCompany(companyName) {
+  const calendars = CalendarApp.getAllOwnedCalendars();
+  const companyCalender = [];
+  for (let i = 0; i < calendars.length; i++) {
+    const calendarsDiscription = calendars[i].getDescription();
+    if (calendarsDiscription.match("就活スケジュール管理")) {
+      companyCalender.push(calendars[i].getName());
+    }
+  }
+  const result = companyCalender.includes(companyName);
+  if (result == false) {
+    return true;
+  } else {
+    return null;
+  }
 }
